c8ebd69f7e9be83f6d7c8a28ffc47a6d8d44ab69
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-06-01 16:12:29 +0200

          

--- android/src/ti/paypal/PaymentItemProxy.java
+++ android/src/ti/paypal/PaymentItemProxy.java
@@ -12,9 +12,9 @@ import ti.paypal.util.PaymentItem;
 @Kroll.proxy(creatableInModule = PaypalModule.class)
 public class PaymentItemProxy extends KrollProxy {
 	private PaymentItem paymentItem;
-	String name = "", sku = "", currency = "EU";
-	BigDecimal price = new BigDecimal(0);
-
+	private String name = "", sku = "", currency = "EU";
+	private BigDecimal price = new BigDecimal(0);
+	int quantify = 1;
 	public PaymentItemProxy() {
 		super();
 	}
@@ -26,52 +26,22 @@ public class PaymentItemProxy extends KrollProxy {
 	// Handle creation options
 	@Override
 	public void handleCreationDict(KrollDict args) {
-		
-		int quantify = 1;
 		if (args.containsKeyAndNotNull("name")) {
-			name = TiConvert.toString(args.get("name"));
+			this.name = TiConvert.toString(args.get("name"));
 		}
 		if (args.containsKeyAndNotNull("price")) {
-			price = new BigDecimal(TiConvert.toString(args.get("price")));
+			this.price = new BigDecimal(TiConvert.toString(args.get("price")));
 		}
 		if (args.containsKeyAndNotNull("sku")) {
-			sku = TiConvert.toString(args.get("sku"));
+			this.sku = TiConvert.toString(args.get("sku"));
 		}
 		if (args.containsKeyAndNotNull("quantify")) {
-			quantify = TiConvert.toInt(args.get("quantify"));
+			this.quantify = TiConvert.toInt(args.get("quantify"));
 		}
 		if (args.containsKeyAndNotNull("currency")) {
-			currency = TiConvert.toString(args.get("currency"));
+			this.currency = TiConvert.toString(args.get("currency"));
 		}
 		
 	}
-	private class PaymentItem {
-		String name = "", sku = "", currency = "EU";
-		BigDecimal price = new BigDecimal(0);
-		int quantify = 1;
-
-		public PaymentItem(KrollDict args) {
-			if (args.containsKeyAndNotNull("name")) {
-				name = TiConvert.toString(args.get("name"));
-			}
-			if (args.containsKeyAndNotNull("price")) {
-				price = new BigDecimal(TiConvert.toString(args.get("price")));
-			}
-			if (args.containsKeyAndNotNull("sku")) {
-				sku = TiConvert.toString(args.get("sku"));
-			}
-			if (args.containsKeyAndNotNull("quantify")) {
-				quantify = TiConvert.toInt(args.get("quantify"));
-			}
-			if (args.containsKeyAndNotNull("currency")) {
-				currency = TiConvert.toString(args.get("currency"));
-			}
-		}
-
-		@Override
-		public String toString() {
-			return "PaymentItem(name:" + name + ", price:" + price + ", sku:" + sku
-					+ ", quantify:" + quantify + ", currency:" + currency + ")";
-		}
-	}
+	
 }
