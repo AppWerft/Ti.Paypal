7bb567721fb83449760563dea4afe95c6e20f72d
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-06-01 16:55:17 +0200

          

--- android/src/ti/paypal/util/Configuration.java
+++ android/src/ti/paypal/util/Configuration.java
@@ -1,27 +1,17 @@
 package ti.paypal.util;
 
 import org.appcelerator.kroll.KrollDict;
-import org.appcelerator.titanium.util.TiConvert;
+
 
 /**
  * Represents a configuration.
  */
-public class Configuration {
+@SuppressWarnings("serial")
+public class Configuration extends KrollDict{
 	String merchantName, merchantPrivacyPolicyURL = "http://google.com",
 			merchantUserAgreementURL = "http://google.com", locale = "en";
 
-	public Configuration(KrollDict args) {
-		if (args.containsKeyAndNotNull("merchantName")) {
-			merchantName = TiConvert.toString(args.get("merchantName"));
-		}
-		if (args.containsKeyAndNotNull("merchantPrivacyPolicyURL")) {
-			merchantPrivacyPolicyURL = TiConvert.toString(args
-					.get("merchantPrivacyPolicyURL"));
-		}
-		if (args.containsKeyAndNotNull("merchantUserAgreementURL")) {
-			merchantUserAgreementURL = TiConvert.toString(args
-					.get("merchantUserAgreementURL"));
-		}
+	public Configuration() {
 	}
 
 	@Override
