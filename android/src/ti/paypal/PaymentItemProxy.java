90910bb8f8cc28e7294789c88482f610956ac25a
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-06-01 14:26:17 +0200

         

--- android/src/ti/paypal/PaymentItemProxy.java
+++ android/src/ti/paypal/PaymentItemProxy.java
@@ -1,22 +1,24 @@
-package ti.paypal; 
- 
-import org.appcelerator.kroll.KrollProxy; 
-import org.appcelerator.kroll.annotations.Kroll; 
- 
-import ti.paypal.util.PaymentItem; 
- 
-@Kroll.proxy 
-public class PaymentItemProxy extends KrollProxy { 
-    private PaymentItem paymentItem; 
-    public PaymentItemProxy(PaymentItem p) { 
-        super(); 
-        paymentItem = p; 
-    } 
-    
+package ti.paypal;
+
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.kroll.annotations.Kroll;
+import ti.paypal.util.PaymentItem;
+
+@Kroll.proxy
+public class PaymentItemProxy extends KrollProxy {
+	private PaymentItem paymentItem;
+
+	public PaymentItemProxy(PaymentItem p) {
+		super();
+		paymentItem = p;
+	}
+
 	@Kroll.method
 	public PaymentItem createPaymentItem() {
 		return paymentItem;
 	}
 
-    public PaymentItem getPaymentItem() { return paymentItem; } 
+	public PaymentItem getPaymentItem() {
+		return paymentItem;
+	}
 }
