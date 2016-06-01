0d97280a56e3113fcf6d0d23480e2446d87816d9
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-06-01 16:53:30 +0200

         

--- android/src/ti/paypal/util/PaymentItem.java
+++ android/src/ti/paypal/util/PaymentItem.java
@@ -1,20 +1,25 @@
 package ti.paypal.util;
 
 import org.appcelerator.kroll.KrollDict;
+
 import java.math.BigDecimal;
 
 /**
  * Represents an PaymentItem.
  */
-public class PaymentItem extends KrollDict{
+@SuppressWarnings("serial")
+public class PaymentItem extends KrollDict {
 	String name = "", sku = "", currency = "EU";
 	BigDecimal price = new BigDecimal(0);
 	int quantify = 1;
+
 	public PaymentItem() {
 		super();
-		String name = "", sku = "", currency = "EU";
-		BigDecimal price = new BigDecimal(0);
-		int quantify = 1;
+		name = "";
+		sku = "";
+		currency = "EU";
+		price = new BigDecimal(0);
+	    quantify = 1;
 	}
 
 	@Override
