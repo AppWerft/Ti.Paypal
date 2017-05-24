6b1bb7be0c62e7ec1bffaa8e0a2957b2dba12bf8
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-05-24 10:08:19 +0200

    v3.0.6

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APPID=ti.paypal
-VERSION=2.0.2
+VERSION=3.0.6
 
 #cp android/assets/* iphone/
 cd android;ant clean; rm -rf build/*;ant ;  unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Library/Application\ Support/Titanium/;cd ..
